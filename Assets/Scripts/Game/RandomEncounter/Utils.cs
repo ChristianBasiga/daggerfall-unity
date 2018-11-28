a607557a5364c39c2a7b6bf37d9eb4eb9a6a6aef
Prince Christian Basiga <princebasiga@gmail.com>
2018-11-28 14:06:36 -0800

    Added to Utils

--- Assets/Scripts/Game/RandomEncounter/Utils.cs
+++ Assets/Scripts/Game/RandomEncounter/Utils.cs
@@ -1,11 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DaggerfallWorkshop.Game;
+
 using DaggerfallWorkshop.Game.UserInterface;
 using DaggerfallWorkshop.Game.UserInterfaceWindows;
 
 namespace DaggerfallRandomEncounterEvents.Utils
 {
+
+
+
+    public class EncounterUtils
+    {
+        //Loading in encouner jsons from resources folder to create prototypes for factory.
+        public static List<string> loadEncounterData()
+        {
+
+            List<string> encounterData = new List<string>();
+
+            Object[] jsonData = Resources.LoadAll("RandomEncounters", typeof(TextAsset));
+
+
+            foreach (Object json in jsonData)
+            {
+                TextAsset textAsset = (TextAsset)json;
+
+                encounterData.Add(textAsset.text);
+            }
+
+            return encounterData;
+
+        }
+
+        public static bool hasActiveSpawn(GameObject[] spawns)
+        {
+            //Cause if null then there wasn't any active objects.
+            return (getActiveSpawn(spawns) != null);
+        }
+
+        public static GameObject getActiveSpawn(GameObject[] spawns)
+        {
+
+            for (int i = 0; i < spawns.Length; ++i)
+            {
+                if (spawns[i].activeInHierarchy)
+                {
+                    return spawns[i];
+                }
+            }
+
+            return null;
+        }
+
+    }
+
+
+
     public class Debugging : MonoBehaviour
     {
 
@@ -24,6 +75,11 @@ namespace DaggerfallRandomEncounterEvents.Utils
 
             debugMessage.SetText(message);
             DaggerfallUI.UIManager.PushWindow(debugMessage);
+        }
+
+        public static void AlertPlayer(string message)
+        {
+            DaggerfallUI.AddHUDText(message, 1.5f);
         }
     }
 }
