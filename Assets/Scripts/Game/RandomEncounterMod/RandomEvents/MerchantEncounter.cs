2cfdabcdf962b7a265d57dd74070bdca802599b4
Prince Christian Basiga <princebasiga@gmail.com>
2018-12-08 22:50:32 -0800

    Updated passive encounter to name merchantEncounter and made it drop loot on death

--- Assets/Scripts/Game/RandomEncounterMod/RandomEvents/MerchantEncounter.cs
+++ Assets/Scripts/Game/RandomEncounterMod/RandomEvents/MerchantEncounter.cs
@@ -12,15 +12,11 @@ using DaggerfallWorkshop.Game.Entity;
 using DaggerfallWorkshop.Game.UserInterfaceWindows;
 namespace DaggerfallRandomEncountersMod.RandomEncounters
 {
-    [RandomEncounterIdentifier(EncounterId = "PassiveEncounter")]
-    public class PassiveEncounter : RandomEncounter
+    [RandomEncounterIdentifier(EncounterId = "MerchantEncounter")]
+    public class MerchantEncounter : RandomEncounter
     {
 
-        bool talkedTo;
-        bool running;
-        StaticNPC merchant;
         GameObject npc;
-        DaggerfallMerchantServicePopupWindow merchantWindow;
 
         //For before tried pickpocketing, quick way to check.
         //this state would actually be stored in filter for how much gold
@@ -35,13 +31,23 @@ namespace DaggerfallRandomEncountersMod.RandomEncounters
 
             warning = "You hear the rustling of items";
 
-            Debug.LogError("Passive encounter bgun");
-
             npc = GameObjectHelper.InstantiatePrefab(DaggerfallUnity.Instance.Option_MobileNPCPrefab.gameObject, "Merchant Encounter", this.transform, GameManager.Instance.PlayerObject.transform.position);
-//            merchant = npc.AddComponent<StaticNPC>();
+
+            //merchant = npc.AddComponent<StaticNPC>();
             //Works, but he has no sprite.
+
             Races npcRace = GameManager.Instance.PlayerGPS.GetRaceOfCurrentRegion();
+
+            //Randomizes texture and gender.
             npc.GetComponent<MobilePersonNPC>().RandomiseNPC(npcRace);
+
+
+            npc.transform.parent = this.transform;
+
+            //This should trigger it
+            //later on I'll make my own npc attack.
+            //npc.AddComponent<EnemyAttack>();
+            //Sets npc to be noble.
 
             //So instance is, but not component, for now I'll set it directly until they fix the prefab themselves.
             npc.GetComponent<MobilePersonNPC>().Motor = npc.GetComponent<MobilePersonMotor>();
@@ -52,6 +58,33 @@ namespace DaggerfallRandomEncountersMod.RandomEncounters
 
             //Gold pieces not get current gold, cause that will include credits.
             goldPlayerHeld = GameManager.Instance.PlayerEntity.GoldPieces;
+
+
+            //If npc dies, then no interaction can happen
+
+            //Why is this not happening?
+            //Works for enemies for sure, but maybe not this.
+            //Oh wait, player entity, que
+            Debug.LogError(npc.GetComponent<DaggerfallEntityBehaviour>().Entity);
+
+            //Why even have this.
+
+            //So that is one issue.
+            npc.GetComponent<DaggerfallEntityBehaviour>().Entity.OnDeath += (DaggerfallEntity entity) =>
+            {
+                //Random chance that dropped gold, offset by pickpocket attempt.
+
+                Debug.LogError("I happen");
+                closure = "He dropped some loot";
+                // Randomise container texture
+                int iconIndex = UnityEngine.Random.Range(0, DaggerfallLootDataTables.randomTreasureIconIndices.Length);
+                int iconRecord = DaggerfallLootDataTables.randomTreasureIconIndices[iconIndex];
+                GameObjectHelper.CreateLootContainer(LootContainerTypes.CorpseMarker, InventoryContainerImages.Merchant,
+                    entity.EntityBehaviour.gameObject.transform.position, null, DaggerfallLootDataTables.randomTreasureArchive,
+                    iconRecord);
+                end();
+            };
+
             base.begin();
         }
 
@@ -66,30 +99,32 @@ namespace DaggerfallRandomEncountersMod.RandomEncounters
             if (npc.GetComponent<MobilePersonNPC>().PickpocketByPlayerAttempted)
             {
 
-                if (goldPlayerHeld != GameManager.Instance.PlayerEntity.GoldPieces)
-                {
-                    Debugging.AlertPlayer("Successfully stole from person");
-                }
-                else
-                {
 
-                    Debugging.AlertPlayer("You try to steal from me? You won't get away with this!");
-                }
                 //Need someway to check if pickpocket worked or not.
                 //Pickocket guaranteed atleast get 1 gold,
                 //So can just check if gold different from before to see if worked.
-
-                npc.GetComponent<DaggerfallEntityBehaviour>().Entity.OnDeath += (DaggerfallEntity entity) =>
+                //Update: Works!
+                if (goldPlayerHeld != GameManager.Instance.PlayerEntity.GoldPieces)
                 {
-                        //If this merchant dies, then cancel.
-                        Debug.LogError("I'm ending it?");
-                    closure = "He didn't drop anything";
+                    //Debugging.AlertPlayer("Successfully stole from person");
+
+                    //Or just let end naturally.
+
+                    closure = "Best not to test your luck and try again.";
                     end();
-                };
+                }
+                else
+                {
+                    Debugging.AlertPlayer("You try to steal from me? You won't get away with this!");
+                    //This will trigger summon of assasin if noble or something.
+                }
+
+
+
+             
 
                 //So doesn't repeat it.
                 npc.GetComponent<MobilePersonNPC>().PickpocketByPlayerAttempted = false;
-
                 //NPC will start running away, need check if pickpocket successful.
             }
 
