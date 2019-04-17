eba48aefecd43cdea152b2c3691a18984060a4a6
xJohneho <40008912+xJohneho@users.noreply.github.com>
2019-04-17 02:25:25 -0700

    Spawn quest Npc
    Work in progress. Need to understand

--- Assets/Scripts/Game/RandomEncounterMod/RandomEvents/CustomNpc.cs
+++ Assets/Scripts/Game/RandomEncounterMod/RandomEvents/CustomNpc.cs
@@ -25,20 +25,11 @@ namespace DaggerfallRandomEncountersMod.RandomEncounters
         {
             warning = "You hear the displeased voices of the masses!?";
 
-            //Method 1
-            //Spawn a custom NPC using Person.cs located in Questing directory
-            //testNPC = DaggerfallWorkshop.Game.Questing.Person.SetupIndividualNPC();
-            //testnpc.addConversationTopics();
-            //addConversationTopics() to modify rumours
-            //Possibly use talkmanager class to modify rumor
-
-            //Method 2
-            //Use previous semesters merchantencounter for pickpocketing.
-            //Change action mode to talk -> open TalkWindow
-            //Change built in rumours/topics
-            DaggerfallWorkshop.Game.Questing.Person testNpc;
             
-            person = GameObjectHelper.InstantiatePrefab(DaggerfallUnity.Instance.Option_MobileNPCPrefab.gameObject, "Merchant Encounter", this.transform, GameManager.Instance.PlayerObject.transform.position);
+            //GameObjectHelper.AddQuestNPC();
+
+            //Also uses the above method however it creates the quest npc in a building
+            //person = GameObjectHelper.AddQuestResourceObjects()
            
             Races npcRace = GameManager.Instance.PlayerGPS.GetRaceOfCurrentRegion();
             person.GetComponent<MobilePersonNPC>().RandomiseNPC(npcRace);
