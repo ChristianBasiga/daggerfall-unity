ac41d54e15f2fe1ab87604f695e708ad30783f81
xJohneho <40008912+xJohneho@users.noreply.github.com>
2019-05-01 00:15:42 -0700

    Creating helper method for QNPC
    Spawn Quest NPC near player. Must modify Place as it prevents spawning quest npcs in the wilderness.

--- Assets/Scripts/Game/RandomEncounterMod/RandomEvents/CustomNpc.cs
+++ Assets/Scripts/Game/RandomEncounterMod/RandomEvents/CustomNpc.cs
@@ -64,6 +64,68 @@ namespace DaggerfallRandomEncountersMod.RandomEncounters
             // Set tag
             go.tag = QuestMachine.questPersonTag;
         }
+        public void placeQNPCNearPlayer(GameObject[] gameObjects, float minDistance = 5f, float maxDistance = 20f)
+        {
+            const float overlapSphereRadius = 0.65f;
+            const float separationDistance = 1.25f;
+            const float maxFloorDistance = 4f;
+
+            // Must have received a valid array
+            if (gameObjects == null || gameObjects.Length == 0)
+                return;
+            // Get roation of spawn ray
+            Quaternion rotation;
+            if (LineOfSightCheck)
+            {
+                // Try to spawn outside of player's field of view
+                float directionAngle = GameManager.Instance.MainCamera.fieldOfView;
+                directionAngle += UnityEngine.Random.Range(0f, 4f);
+                if (UnityEngine.Random.Range(0f, 1f) > 0.5f)
+                    rotation = Quaternion.Euler(0, -directionAngle, 0);
+                else
+                    rotation = Quaternion.Euler(0, directionAngle, 0);
+            else
+            {
+                // Don't care about player's field of view (e.g. at rest)
+                rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 361), 0);
+            }
+
+            // Get direction vector and create a new ray
+            Vector3 angle = (rotation * Vector3.forward).normalized;
+            Vector3 spawnDirection = GameManager.Instance.PlayerObject.transform.TransformDirection(angle).normalized;
+            Ray ray = new Ray(GameManager.Instance.PlayerObject.transform.position, spawnDirection);
+
+            // Check for a hit
+            Vector3 currentPoint;
+            RaycastHit initialHit;
+            if (Physics.Raycast(ray, out initialHit, maxDistance))
+            {
+                // Separate out from hit point
+                float extraDistance = UnityEngine.Random.Range(0f, 2f);
+                currentPoint = initialHit.point + initialHit.normal.normalized * (separationDistance + extraDistance);
+
+                // Must be greater than minDistance
+                if (initialHit.distance < minDistance)
+                    return;
+            }
+            else
+            {
+                // Player might be in an open area (e.g. outdoors) pick a random point along spawn direction
+                currentPoint = GameManager.Instance.PlayerObject.transform.position + spawnDirection * UnityEngine.Random.Range(minDistance, maxDistance);
+            }
+
+            // Must be able to find a surface below
+            RaycastHit floorHit;
+            ray = new Ray(currentPoint, Vector3.down);
+            if (!Physics.Raycast(ray, out floorHit, maxFloorDistance))
+                return;
+
+            // Ensure this is open space
+            Vector3 testPoint = floorHit.point + Vector3.up * separationDistance;
+            Collider[] colliders = Physics.OverlapSphere(testPoint, overlapSphereRadius);
+            if (colliders.Length > 0)
+                return;
+        }
         public override void begin()
         {
            
